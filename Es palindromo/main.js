0caf73623b0ef13cf0129698cbaf7ccec19100e9
DavideL90 <davide.lecci@icloud.com>
2018-03-14 20:53:41 +0100

    completed palindroma

---
+++ Es palindromo/main.js
@@ -0,0 +1,10 @@
+var wordInput = prompt("Scrivi una parola:");
+var reverseWord = "";
+document.getElementById("inputword").innerHTML = wordInput;
+for (var i = 0; i < wordInput.length; i++) {
+  var a = wordInput.charAt(wordInput.length - (i+1));
+  reverseWord = reverseWord + a;
+}
+if(wordInput === reverseWord){
+  document.write("La parola è palindroma");
+}
